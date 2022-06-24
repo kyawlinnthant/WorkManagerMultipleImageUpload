56cdd2c615997bd189719c3c729b16daf76f344d
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2022-06-24 16:11:37 +0630

    add a space

--- app/src/main/java/com/klt/workmanagertest/ui/MainActivity.kt
+++ app/src/main/java/com/klt/workmanagertest/ui/MainActivity.kt
@@ -14,6 +14,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
             WorkManagerTestTheme {
                 // A surface container using the 'background' color from the theme
